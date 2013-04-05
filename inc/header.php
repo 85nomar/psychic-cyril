d259396407e65ef2a69a92decf9c63ed5c89c248
Edi <github@mediafreak.ch>
2013-04-05 21:54:56 +0200

    added basic header template and .gitignore

---
+++ inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Content-Language" content="DE">
+        <title> Verkaufen </title>
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel="stylesheet" type="text/css" href="/lib/css/bootstrap.css">
+        <script type="text/javascript" src="/lib/js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="/lib/js/bootstrap.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
