f04830694df560eaf92e621eac197b2919e8198b
Edi <github@mediafreak.ch>
2013-04-06 12:05:32 +0200

    changed filetype. added multiple select inputs

---
+++ step1.php
@@ -0,0 +1,51 @@
+<div class="row-fluid">
+    <div class="span12">
+        <form class="form-search">
+            <div class="input-append">
+                <input class="span4" id="appendedInputButton" type="text">
+                <button class="btn" type="button">
+                    Find category!
+                </button>
+            </div>
+        </form>
+    </div>
+    <div class="span12">
+        <div class="controls controls-row">
+            <select multiple="multiple" class="span2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <select multiple="multiple" class="span2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <select multiple="multiple" class="span2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <select multiple="multiple" class="span2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <select multiple="multiple" class="span2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+    </div>
+</div>
