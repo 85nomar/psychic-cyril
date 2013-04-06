df76d6b83479af31535bf37e1068af0dc26a357d
Edi <github@mediafreak.ch>
2013-04-06 12:37:07 +0200

    added horizontal navbar

---
+++ horizontal_nav.php
@@ -0,0 +1,17 @@
+<ol class="nav nav-pills">
+    <li class="active">
+        <a href="#">Category selection</a>
+    </li>
+    <li>
+        <a href="#">Product details</a>
+    </li>
+    <li>
+        <a href="#">Pricing & Duration</a>
+    </li>
+    <li>
+        <a href="#">Shipping details</a>
+    </li>
+    <li>
+        <a href="#">Review</a>
+    </li>
+</ol>
