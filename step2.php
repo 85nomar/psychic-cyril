8369420131cc4ee6b72d230861dbc5fb8e21fe80
Edi <github@mediafreak.ch>
2013-04-06 16:37:36 +0200

    new form alignment. added images placeholders

--- step2.php
+++ step2.php
@@ -1,6 +1,8 @@
-<?php require('inc/layout.php');
-$Layout->title = 'Selling';
-$Layout->start(); ?>
+<?php
+require ('inc/layout.php');
+$Layout->title='Selling';
+$Layout->start();
+?>
 
 <div class="row-fluid">
     <div class="span10">
@@ -21,7 +23,7 @@ $Layout->start(); ?>
     </div>
 </div>
 <div class="accordion" id="accordion2">
-    <form class="form-horizontal">
+    <form>
         <div class="accordion-group">
             <div class="accordion-heading">
                 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne"> Basic attributes </a>
@@ -30,43 +32,32 @@ $Layout->start(); ?>
                 <div class="accordion-inner">
                     <div class="row-fluid">
                         <div class="span12">
-
-                            <div class="control-group">
-                                <div class="controls">
-                                    <label class="checkbox">
-                                        <input type="checkbox">
-                                        Setup your listing in two languages </label>
+                            <label class="checkbox">
+                                <input type="checkbox">
+                                Setup your listing in two languages </label>
+                            <label for="inputTitle">Title</label>
+                            <input class="span6" type="text" id="inputTitle" placeholder="Insert the name of your product here">
+                            <label for="inputSubtitle">Sub-Title</label>
+                            <input class="span6" type="text" id="inputSubtitle" placeholder="Tease your product here">
+                            <label for="inputDescription">Description</label>
+                            <textarea class="span12" rows="4" placeholder="Describe your product here"></textarea>
+                            <label for="inputPictures">Pictures</label>
+                            <div class="row-fluid">
+                                <div class="span3">
+                                    <img src="img/img_placeholder_140.jpg" class="img-polaroid" style="width:140px;height:140px;">
+                                </div>
+                                <div class="span9">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
+                                    <img src="img/img_placeholder_90.jpg" class="img-polaroid" style="width:90px;height:90px;">
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label" for="inputTitle">Title</label>
-                                <div class="controls">
-                                    <input type="text" id="inputTitle" placeholder="Insert the name of your product here">
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <label class="control-label" for="inputSubtitle">Sub-Title</label>
-                                <div class="controls">
-                                    <input type="text" id="inputSubtitle" placeholder="Tease your product here">
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <label class="control-label" for="inputDescription">Description</label>
-                                <div class="controls">
-                                    <textarea rows="4">Describe your product here</textarea>
-                                </div>
-                            </div>
-
-                            <div class="control-group">
-                                <label class="control-label" for="inputPictures">Pictures</label>
-                                <div class="controls">
-                                    <img src="#" class="img-polaroid" style="width:140px;height:140px;">
-                                    <img src="#" class="img-polaroid" style="width:90px;height:90px;">
-                                    <img src="#" class="img-polaroid" style="width:90px;height:90px;">
-                                    <img src="#" class="img-polaroid" style="width:90px;height:90px;">
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
@@ -74,18 +65,14 @@ $Layout->start(); ?>
         </div>
         <div class="accordion-group">
             <div class="accordion-heading">
-                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo"> Advanced attributes </a>
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo"> Advanced attributes <span class="muted">(optional)</span></a>
             </div>
             <div id="collapseTwo" class="accordion-body collapse">
                 <div class="accordion-inner">
                     <div class="row-fluid">
                         <div class="span12">
-                            <div class="control-group">
-                                <label class="control-label" for="inputNumber">Number of Products</label>
-                                <div class="controls">
-                                    <input type="number" id="inputNumber" placeholder="1">
-                                </div>
-                            </div>
+                            <label for="inputNumber">Number of Products</label>
+                            <input class="span1" type="number" id="inputNumber" placeholder="1">
                         </div>
                     </div>
                 </div>
@@ -93,5 +80,6 @@ $Layout->start(); ?>
         </div>
     </form>
 </div>
+<a href="#" class="btn btn-primary">Forward to set Pricing & Duration</a><span class="separator muted"> | </span><a href="#" class="btn btn-link">Back to choose category</a>
 
 <?php $Layout->end(); ?>
