139b945790f6973ea92d149e1a29989d6930f1f5
Nobel Martinez <85nomar@gmail.com>
2013-04-06 14:07:02 +0200

    - fonts added - removed error generating files

--- inc/head.php
+++ inc/head.php
@@ -3,15 +3,18 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="Content-Language" content="DE">
-        <title> Selling </title>
-
+        <title> Selling </title>        
+ 
         <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
         <script type="text/javascript" src="lib/js/jquery-1.9.1.js"></script>
         <script type="text/javascript" src="lib/js/bootstrap.js"></script>
+        
+
 
         <!-- Stylesheets -->
         <link rel="stylesheet" type="text/css" href="css/ric_default.css">
         <link rel="stylesheet" type="text/css" href="css/main.css">
+
     </head>
     <body>
         <div class="container">
