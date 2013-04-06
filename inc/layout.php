fea841bfb20144868a5f1415d17bf4aad32d67b8
Edi <github@mediafreak.ch>
2013-04-06 14:50:36 +0200

    improved rendering of navigation

--- inc/layout.php
+++ inc/layout.php
@@ -1,60 +1,54 @@
 <?php
+class Layout
+{
+    public $anonym=false;
+    public $title='';
+    // Main Navigation
+    public $formNav=array(
+        array(
+            'url'=>'#',
+            'title'=>'Step1',
+        ),
+        array(
+            'url'=>'#',
+            'title'=>'Step2',
+        ),
+        array(
+            'url'=>'#',
+            'title'=>'Step3',
+        ),
+        array(
+            'url'=>'#',
+            'title'=>'Step4',
+        ),
+        array(
+            'url'=>'#',
+            'title'=>'Step5',
+        )
+    );
+    public $navTitle='';
+    public $prependScript='';
+    public $appendScript='';
+    public $prependCSS='';
+    public $appendCSS='';
+    public $isAdmin=false;
+    function __construct() {
+        date_default_timezone_set('UTC');
+        $this->navTitle=basename($_SERVER['PHP_SELF'],".php");
+    }
 
-	class Layout {
-		public $anonym = false;
-		public $title = '';
+    // Layout listed Contructor
+    public function start() {
+        require ('head.php');
+        require ('header.php');
+        require ('formnav.php');
+    }
 
+    public function end() {
+        require ('footer.php');
+    }
 
-		// Main Navigation
-		public $formNav = array(
-			'Form step 1' => array(
-				'url' => '#'
-			),
-			'Form step 2' => array(
-				'url' => '#'
-			),
-			'Form step 3' => array(
-				'url' => '#'
-			),
-			'Form step 4' => array(
-				'url' => '#'
-			),
-			'Form step 5' => array(
-				'url' => '#'
-			)
+}
 
-
-		);
-
-
-		public $navTitle = '';
-
-		public $prependScript = '';
-		public $appendScript = '';
-
-		public $prependCSS = '';
-		public $appendCSS = '';
-
-		public $isAdmin = false;
-
-		function __construct() {
-			date_default_timezone_set('UTC'); 
-			$this->navTitle = basename($_SERVER['PHP_SELF'], ".php");
-		}
-
-
-		// Layout listed Contructor
-		public function start() {
-			require('head.php');
-			require('header.php');
-			require('formnav.php');
-		}
-
-		public function end() {
-			require('footer.php');
-		}
-
-	} 
-
-	$Layout = new Layout();
+$Layout=new Layout();
 ?>
