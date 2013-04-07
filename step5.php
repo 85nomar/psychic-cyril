13819bf9c6cb02c470d345353b4b886805ce666b
Edi <github@mediafreak.ch>
2013-04-07 22:32:19 +0200

    added and started to layout step5

--- step5.php
+++ step5.php
@@ -1,7 +1,7 @@
 <?php
 require ('inc/layout.php');
-$Layout->title='Selling';
-$Layout->start();
+$Layout -> title = 'Selling';
+$Layout -> start();
 ?>
 
 <h2>Review your listing</h2>
@@ -25,8 +25,120 @@ $Layout->start();
     </div>
 </div>
 
-<div class="form-actions">
-<a href="#" class="btn btn-success">List your item</a><span class="separator muted"> | </span><a href="#" class="btn btn-link">Back to set Shipping Options</a>
+<div class="row-fluid">
+    <div class="span3">
+        <div class="row-fluid section">
+            <div class="span12 text-center">
+                <img src="img/img_placeholder_140.jpg" class="img-polaroid">
+            </div>
+        </div>
+        <div class="row-fluid section">
+            <div class="span12 text-center">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+                <img src="img/img_placeholder_90.jpg" class="img-polaroid">
+            </div>
+        </div>
+    </div>
+    <div class="span9">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend>
+                    Product details
+                </legend>
+                <div class="row-fluid">
+                    <div class="span10">
+                        <div class="row-fluid">
+                        <table class="table table-condensed">
+                            <tbody>
+                                <tr>
+                                    <td class="span3">Language</td>
+                                    <td class="span9">German</td>
+                                </tr>
+                                <tr>
+                                    <td>Title</td>
+                                    <td>i-Phone 3 GS 16GB black</td>
+                                </tr>
+                                <tr>
+                                    <td>Sub-Title</td>
+                                    <td>Brand new i-Phone 3 GS the one and only</td>
+                                </tr>
+                                <tr>
+                                    <td>Description</td>
+                                    <td>Lorum ipsum dolores Lorum ipsum dolores Lorum ipsum dolores Lorum ipsum doloresLorum ipsum dolores Lorum ipsum dolores
+                                    Lorum ipsum dolores Lorum ipsum dolores Lorum ipsum doloresLorum ipsum dolores</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                    <div class="span2 text-right">
+                        <button class="btn">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend>
+                    Pricing & duration
+                </legend>
+                <div class="row-fluid">
+                    <div class="span10"></div>
+                    <div class="span2 text-right">
+                        <button class="btn">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend>
+                    Shipping
+                </legend>
+                <div class="row-fluid">
+                    <div class="span10"></div>
+                    <div class="span2 text-right">
+                        <button class="btn">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+            </fieldset>
+            <h4>Fees</h4>
+            <table class="table table-condensed table-striped table-sum">
+                <tbody>
+                    <tr>
+                        <td>Listing fees</td>
+                        <td>0.05 CHF</td>
+                    </tr>
+                    <tr>
+                        <td>Buy now option</td>
+                        <td>1.00 CHF</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>1.05 CHF</td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
 </div>
 
-<?php $Layout->end(); ?>
+<div class="form-actions">
+    <a href="#" class="btn btn-success">List your item</a>
+    <span class="separator muted"> | </span>
+    <a href="#" class="btn">Preview</a>
+    <span class="separator muted"> | </span>
+    <a href="#" class="btn btn-link">Back to set Shipping Options</a>
+</div>
+
+<?php $Layout -> end();
+?>
