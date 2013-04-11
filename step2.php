e32e9ebeb0fc73f41ca2cfac52f13a326439ec36
Edgar Schnueriger <edgar.schnueriger@ricardo.ch>
2013-04-11 12:41:11 +0200

    added editor btns

--- step2.php
+++ step2.php
@@ -9,27 +9,22 @@ $Layout -> start();
         <div class="span12 well">
             <label class="checkbox">
                 <input type="checkbox">
-                Setup your listing in two languages
-            </label>
+                Setup your listing in two languages </label>
         </div>
     </div>
     <div class="row-fluid">
         <div class="span12 well">
-            <label for="inputTitle">
-                Title
-            </label>
+            <label for="inputTitle"> Title </label>
             <input class="span5" type="text" id="inputTitle" placeholder="Insert the name of your product here">
-            <label for="inputSubtitle">
-                Sub-Title
-            </label>
+            <label for="inputSubtitle"> Sub-Title </label>
             <input class="span7" type="text" id="inputSubtitle" placeholder="Tease your product here">
-            <label for="inputDescription">
-                Description
-            </label>
+            <label for="inputDescription"> Description </label>
+            <div class="editor-toolbar pull-right">
+                <a href="#">more</a>
+                <i class="icon-bold"></i><i class="icon-italic"></i><i class="icon-list"></i>
+            </div>
             <textarea class="span12" rows="4" placeholder="Describe your product here"></textarea>
-            <label for="inputPictures">
-                Pictures
-            </label>
+            <label for="inputPictures"> Pictures </label>
             <div class="row-fluid">
                 <div class="span3">
                     <img src="img/img_placeholder_140.jpg" class="img-polaroid">
@@ -56,26 +51,18 @@ $Layout -> start();
             <div id="collapseTwo" class="accordion-body collapse">
                 <div class="accordion-inner">
                     <div class="row-fluid">
-                        <label for="inputProductCondition">
-                            Product condition
-                        </label>
+                        <label for="inputProductCondition"> Product condition </label>
                         <select id="productCondition">
                             <option>New</option>
                             <option>Original</option>
                             <option>Used</option>
                             <option>Broken</option>
                         </select>
-                        <label for="inputNumber">
-                            Number of Products
-                        </label>
+                        <label for="inputNumber"> Number of Products </label>
                         <input class="span1" type="number" id="inputNumber" placeholder="1">
-                        <label for="inputUPCnumber">
-                            UPC, ISBN or your own product number
-                        </label>
+                        <label for="inputUPCnumber"> UPC, ISBN or your own product number </label>
                         <input class="span4" type="text" id="inputUPCnumber" placeholder="UPC, ISBN or other product number">
-                        <label for="inputWarranty">
-                            Warranty description
-                        </label>
+                        <label for="inputWarranty"> Warranty description </label>
                         <select id="warranty">
                             <option>No warranty</option>
                             <option>As described</option>
