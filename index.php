6b1239d6c1abcfe55e25b0df3b41166304badc8b
Soeren <soeren.klippel@ricardo.ch>
2013-05-28 14:03:22 +0200

    ReadyState 2.1

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ $Layout->start(); ?>
 <h2>Welchen Prototypen möchten Sie testen?</h2>
 
 <div class="row-fluid" id="protoTypeSelector" style="text-align:center;">
-	<a href="step1.php" data-view="core" class="btn btn-large btn-warning setCookie">Core</a>
-	<a href="step1.php" data-view="cars" class="btn btn-large btn-primary setCookie">Cars & Bikes</a>
+	<a href="step1.php" data-view="core" class="btn btn-large btn-warning setCookie span3 offset3">ricardo.ch</a>
+	<a href="step1.php" data-view="cars" class="btn btn-large btn-primary setCookie span3">autoricardo.ch</a>
 </div>
+
+<script type="text/javascript">
+	setCookie('catg','',-1);
+</script>
 
 <?php $Layout->end(); ?>
